936087034867ac3005226a8294ba5258ab568d5f
Manuel Aristarán <jazzido@jazzido.com>
2017-01-26 18:07:21 -0500

    starting with parser

--- thegamma-service.rb
+++ thegamma-service.rb
@@ -32,6 +32,44 @@ def table_metadata(table_name)
   }
 end
 
+def parse_action
+
+end
+
+def parse_args(s)
+  p = s.chars.reduce([false, [], []]) { |memo, c|
+    quoted, current, acc = memo
+    if c == "'" && quoted
+      [false, c + current, acc]
+    elsif c == "'" && !quoted
+      [true, c + current, acc]
+    elsif c == "," && !quoted
+      [quoted, [], [current.reverse.join] + acc]
+    else
+      [quoted, [c] + current, acc]
+    end
+  }
+
+  ([p[1].reverse.join] + p[2]).reverse
+end
+
+def parse_chunk(s)
+  open_par, close_par = s.index('('), s.rindex(')')
+  if open_par.nil? || close_par.nil?
+    [s, []]
+  else
+    [s[0..(open_par-1)], parse_args(s[(open_par+1)..(close_par-1)])]
+  end
+end
+
+def parse_qs
+  chunks = env['QUERY_STRING'].split('$').map { |c| parse_chunk(c) }
+  if chunks.size == 0
+  else
+
+  end
+end
+
 Cuba.define do
 
   on get do
@@ -40,7 +78,7 @@ Cuba.define do
       tbl = ActiveRecord::Base.connection.tables
         .find { |t| t == table }
 
-      return if tbl.nil?
+      qs = env['QUERY_STRING']
 
       res.write table_metadata(tbl).to_json
     end
